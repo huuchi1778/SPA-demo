df653c73b69ffc32a7b530d5e863f8946755cb16
Chi Nguyen <nguyenhuuchi1999@gmail.com>
2023-03-26 00:39:48 +0700

    Setup project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,14 @@
 module.exports = {
-  extends: '@cybozu/eslint-config/presets/es5'
+  extends: '@cybozu/eslint-config/presets/es5',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  env: {
+    es6: true
+  },
+  globals: {
+    process: 'readonly',
+    __dirname: 'readonly'
+  }
 };
